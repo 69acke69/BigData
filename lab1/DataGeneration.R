545c2996e839115426ed7a72a83e2fcc026967c0
gurra <gurra.fellander@gmail.com>
2023-11-09 10:37:36 +0100

    fix

--- lab1/DataGeneration.R
+++ lab1/DataGeneration.R
@@ -7,13 +7,25 @@ x2 <- runif(N,-1,1)
 X <- cbind(x1,x2)
 y <- ifelse(x2>x1,-1,1)
 
-plot(x1,x2)
 
-w = c(1,0)
+xdata <- seq(-1, 1, length = 100)
+w = c(1,1)
+f = function(x) {-w[1]/w[2]*x}
+
+plot(xdata, f(xdata), type='l')
+arrows(0,0,w[1]/5,w[2]/5)
+
+points(x1,x2)
+
+
 
 for(i in 1:N) {
   yhat = w%*%X[i,]
   w = w + sign(yhat)%*%X[i,]
+  
+  plot(xdata, f(xdata), type='l')
+  arrows(0,0,w[1]/5,w[2]/5)
+  
 } 
 
 arrows(0,0,w[1],w[2])
