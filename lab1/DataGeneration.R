325c4a807754026eacf3cf557d13fb868f65861a
Axel Eriksson <ens18aen@cs.umu.se>
2023-11-13 10:35:55 +0100

    Code for Task1! Name of file bad

--- lab1/DataGeneration.R
+++ lab1/DataGeneration.R
@@ -1,5 +1,4 @@
 rm(list=ls())
-
 set.seed(201606)
 N <- 20
 x1 <- runif(N,-1,1)
@@ -7,30 +6,32 @@ x2 <- runif(N,-1,1)
 X <- cbind(x1,x2)
 y <- ifelse(x2>x1,-1,1)
 
+#Plot the two different groups 
+colors <- ifelse(y == 1, "blue", "red")
+plot(X[, 1], X[, 2], col = colors, pch = 16, main = "Scatter Plot with Different Colors", xlab = "X1", ylab = "X2")
 
-xdata <- seq(-1, 1, length = 100)
-w = c(1,1)
-f = function(x) {-w[1]/w[2]*x}
+#Add ones
+X <- cbind(1,x1,x2)
+#Initial guess for weights
+w <- c(1,1,1)
 
-plot(xdata, f(xdata), type='l')
-arrows(0,0,w[1]/5,w[2]/5)
+max_iter = 100
+for(j in 1:max_iter){
+  misclassified <- 0
+  for(i in 1:N){
+    if(sign(w%*%X[i,]) != y[i]) {
+      #misclassified !! Update weights!! w
+      w <- w + y[i]%*% X[i,]
+      misclassified <- misclassified + 1
+    }
+  }
+  if(misclassified == 0) {
+    cat("Converged in ",j)
+    break
+  }
+}
 
-points(x1,x2)
-##
-##
-##
-##
-#JAAAAAG ÄR TOKBAKIS. JAG GÅR HEM! VI LÖSER DET HÄR FRAMÖVER
-##
-###
-##
-for(i in 1:N) {
-  yhat = w%*%X[i,]
-  w = w + sign(yhat)%*%X[i,]
-  
-  plot(xdata, f(xdata), type='l')
-  arrows(0,0,w[1]/5,w[2]/5)
-  
-} 
-
-arrows(0,0,w[1],w[2])
+#Verify that the obtained weights defines a good decision boundary 
+x_values <- seq(-1, 1, length.out = 100)
+y_values <- (-w[1] - w[2] * x_values) / w[3]
+lines(x_values, y_values, col = "green", lwd = 2)
